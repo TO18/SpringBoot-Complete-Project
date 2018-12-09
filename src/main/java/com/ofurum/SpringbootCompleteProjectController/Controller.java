9cdb719e85a4e2d93a313d9550e0109b21b68505
ThankGod Ofurum <thankgodofurum@ThankGods-MBP.attlocal.net>
2018-12-09 05:42:41 -0600

    created GET model for request object mapping with spring API

--- src/main/java/com/ofurum/SpringbootCompleteProjectController/Controller.java
+++ src/main/java/com/ofurum/SpringbootCompleteProjectController/Controller.java
@@ -1,16 +1,21 @@
 package com.ofurum.SpringbootCompleteProjectController;
 
+import com.ofurum.SpringbootCompleteProjectModel.GetUserRequestModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
+@RequestMapping("/users")
 public class Controller {
 
-    @GetMapping("/users")
-    public ResponseEntity<?> getAllUser() {
-        return new ResponseEntity<>("All Users", HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<?> getAllUser(@Valid GetUserRequestModel getUserRequestModel) {
+        return new ResponseEntity<>(getUserRequestModel, HttpStatus.OK);
     }
 
 }
